a042b85227ae2797ac5d3a2d8665ccf510d1eb1a
ElenaPupysheva <nyankem@gmail.com>
2025-12-10 13:28:04 +0300

    Настройка шрифта, цветов и темы приложения

--- app/src/main/java/com/alonso/onetapwallet/ui/theme/Color.kt
+++ app/src/main/java/com/alonso/onetapwallet/ui/theme/Color.kt
@@ -1,11 +1,9 @@
 package com.alonso.onetapwallet.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryBlack = Color(0xFF070707)
+val PrimaryPurple = Color(0xFF6552FE)
+val SecondaryGreen = Color(0xFF48D49E)
+val PrimaryGrey = Color(0xFF6C757D)
+val PrimaryWhite = Color(0xFFFFFFFF)
+val SecondaryOrange = Color(0xFFFF8266)
